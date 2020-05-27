ebf975220ef55caaaa2e3a0165830cfa816a7cb4
viniciuspalves1@hotmail.com <viniciuspalves1@hotmail.com>
2020-05-27 14:55:40 -0300

    Adicionando rota teste

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 
-app.use((req, res, next) => {
+app.use('/teste', (req, res, next) => {
     res.status(200).json({
         mensagem: 'Deu certo!'
     })
