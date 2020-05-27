d6b8ecb5ba4e55e1c05cb33a1384c252232749ac
viniciuspalves1@hotmail.com <viniciuspalves1@hotmail.com>
2020-05-27 17:10:43 -0300

    adicionando constante login

--- routes/dono.js
+++ routes/dono.js
@@ -1,16 +1,7 @@
 const express = require('express');
 const router = express.Router();
+const Login = require("../middleware/login-middleware");
 
-router.get('/', (req, res, next) => {
-    res.status(200).send({
-        mensagem: 'Usando o GET dentro da rota do dono'
-    })
-});
 
-router.post('/', (req, res, next) => {
-    res.status(201).send({
-        mensagem: 'Usando o POST dentro da rota de produtos'
-    })
-});
 
 module.exports = router;
