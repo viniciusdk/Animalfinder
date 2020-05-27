f41d614f7b62b235305f893a8668f6a711b753b2
viniciuspalves1@hotmail.com <viniciuspalves1@hotmail.com>
2020-05-27 17:15:55 -0300

    Adicionando controle de acesso e rotas

--- app.js
+++ app.js
@@ -2,7 +2,44 @@ const express = require('express');
 const app = express();
 
 const rotaDonos = require('./routes/dono');
+const rotaAnimal = require('./routes/animal'); 
 
-app.use('/produtos', rotaDonos);
+app.use(morgan('dev'));
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*'); // allow access from everywhere
+    res.header(
+        'Access-Control-Allow-Headers',
+        'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+    );
+
+    if (req.method === 'OPTIONS') {
+        res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET'); // Quais os métodos possíveis
+        return res.status(200).json({});
+    }
+
+    next();
+});
+
+app.use('/donos', rotaDonos);
+app.use('/animais', userRoutes);
+
+app.use((req, res, next) => {
+    const error = new Error('Not found...');
+    error.status = 404;
+    next(error);
+});
+
+app.use((error, req, res, next) => {
+    res.status(error.status || 500);
+    res.json({
+        error: {
+            message: error.message
+        }
+    });
+});
 
 module.exports = app; 
