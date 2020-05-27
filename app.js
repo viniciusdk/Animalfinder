61857f0127060a65676a47818c068129cd77ef5a
viniciuspalves1@hotmail.com <viniciuspalves1@hotmail.com>
2020-05-27 15:00:51 -0300

    Adicionando rotas

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const app = express();
 
+const rotaDonos = require('./routes/dono');
+
+app.use('/produtos', rotaDonos);
+
 app.use('/teste', (req, res, next) => {
     res.status(200).json({
         mensagem: 'Deu certo!'
