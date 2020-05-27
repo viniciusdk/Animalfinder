217da68b685d6009e030022348d58b488ad6058e
viniciuspalves1@hotmail.com <viniciuspalves1@hotmail.com>
2020-05-27 18:02:50 -0300

    Removendo comentarios

--- app.js
+++ app.js
@@ -12,14 +12,14 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*'); // allow access from everywhere
+    res.header('Access-Control-Allow-Origin', '*'); 
     res.header(
         'Access-Control-Allow-Headers',
         'Origin, X-Requested-With, Content-Type, Accept, Authorization'
     );
 
     if (req.method === 'OPTIONS') {
-        res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET'); // Quais os métodos possíveis
+        res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
         return res.status(200).json({});
     }
 
