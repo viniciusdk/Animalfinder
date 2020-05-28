24e0cae4be30a5f154fee1604631ebc90e131412
viniciuspalves1@hotmail.com <viniciuspalves1@hotmail.com>
2020-05-28 14:14:18 -0300

    Adicionando login

--- routes/dono-route.js
+++ routes/dono-route.js
@@ -6,6 +6,7 @@ const Login = require("../middleware/login-middleware");
 const DonoController = require("../controllers/dono-controller");
 
 router.post("/create", DonoController.createDono);
+router.post("/login", DonoController.login);
 
 
 module.exports = router;
