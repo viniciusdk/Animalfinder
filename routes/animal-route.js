142240aa6ccdca070e1cea4b999b08c6719f7b17
viniciuspalves1@hotmail.com <viniciuspalves1@hotmail.com>
2020-05-28 15:32:14 -0300

    Adicionando rota

--- routes/animal-route.js
+++ routes/animal-route.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const Login = require("../middleware/login-middleware");
 
+const AnimalController = require("../controllers/animal-controller");
 
+router.post("/cadastro", Login.required, AnimalController.cadastrarAnimal);
 
 module.exports = router;
