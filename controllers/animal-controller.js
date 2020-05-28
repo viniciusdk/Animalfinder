5cff92cbb185216ea5799e821571ec3347a26f01
viniciuspalves1@hotmail.com <viniciuspalves1@hotmail.com>
2020-05-28 15:31:36 -0300

    Adicionando query para cadastrar animal

--- controllers/animal-controller.js
+++ controllers/animal-controller.js
@@ -2,3 +2,20 @@ const mysql = require('../mysql').pool;
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 require("dotenv").config();
+
+exports.cadastrarAnimal = (req, res, next) => {
+
+    let aml = req.body;
+    mysql.getConnection((error1, conn) => {
+      conn.query(`insert into Animal (foto, nome, idade, informacoes_extras, cidade, estado, status, id_dono) values (?, ?, ?, ?, ?, ?, ?, ?)`,
+      [aml.foto, aml.nome, aml.idade, aml.informacoes_extras, aml.cidade, aml.estado, aml.status, res.locals.id_dono],
+        (error, results, fields) => {
+          conn.release();
+          if (error) {
+            res.status(500).send({ error: error });
+          } else {
+            res.status(201).send({ message: "Animal inserido com Sucesso." });
+          }
+        });
+    });
+};
