3be1d318efe45c96ec071f15f700727b55653be5
viniciuspalves1@hotmail.com <viniciuspalves1@hotmail.com>
2020-05-28 15:32:05 -0300

    alterando nome

--- middleware/login-middleware.js
+++ middleware/login-middleware.js
@@ -15,16 +15,16 @@ exports.optional = (req, res, next) => {
 };
 
 exports.required = (req, res, next) => {
-    res.locals.id = 0;
+    res.locals.id_dono = 0;
     try {
         const token = req.headers.authorization.split(" ")[1];
         const decoded = jwt.verify(token, process.env.JWT_KEY);
         req.userData = decoded;
-        res.locals.id = req.userData.id;
+        res.locals.id_dono = req.userData.id_dono;
         next();
     } catch (error) {
         return res.status(401).json({
-            message: 'Usuário não autenticado.'
+            message: '111111Usuário não autenticado.'
         });
     }
 };
