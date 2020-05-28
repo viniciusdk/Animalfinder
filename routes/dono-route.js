b49da18e37910c3291e5390530903fac0e7363e9
viniciuspalves1@hotmail.com <viniciuspalves1@hotmail.com>
2020-05-28 13:53:34 -0300

    Adicionando rota de criação de conta

--- routes/dono-route.js
+++ routes/dono-route.js
@@ -3,5 +3,9 @@ const router = express.Router();
 const Login = require("../middleware/login-middleware");
 
 
+const DonoController = require("../controllers/dono-controller");
+
+router.post("/create", DonoController.createDono);
+
 
 module.exports = router;
